d7af5be22e1e5fc772701e9db0bff9ee4f9327b1
Draicor <62912849+draicor@users.noreply.github.com>
2024-12-06 13:51:42 -0400

    06/12/2024
    - Minor updates

--- client/main.gd
+++ client/main.gd
@@ -4,6 +4,17 @@ const packets := preload("res://packets.gd")
 
 func _ready() -> void:
 	var new_packet := packets.Packet.new()
-	new_packet.from_bytes([8, 69, 18, 15, 10, 13, 72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100, 33])
+	# new_packet.from_bytes([8, 69, 18, 15, 10, 13, 72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100, 33])
+	new_packet.set_sender_id(1)
+	new_packet.new_chat_message().set_text("Hey there")
+	
 	print(new_packet)
 	print(new_packet.has_chat_message())
+	print(new_packet.to_bytes())
+	
+	if new_packet.has_chat_message():
+		pass
+	elif new_packet.has_client_id():
+		pass
+	else:
+		pass
--- server/cmd/main.go
+++ server/cmd/main.go
@@ -14,6 +14,18 @@ func main() {
 
 	fmt.Println(packet)
 
+	// Determines the type of payload at runtime
+	switch packet.GetPayload().(type) {
+	case *packets.Packet_ChatMessage:
+		fmt.Println("Its a chat message.")
+	case *packets.Packet_ClientId:
+		fmt.Println("It's a client ID message.")
+	case nil:
+		fmt.Println("The payload was not set.")
+	default:
+		fmt.Println("Invalid payload.")
+	}
+
 	// [8 1 18 14 10 12 72 101 108 108 111 32 87 111 114 108 100 33]
 	// [8 1 26 3 8 244 3]
 	//data := []byte{8, 1, 18, 14, 10, 12, 72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33}
