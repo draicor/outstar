13e198341430757ad83a31f3fa476c2979730cd7
Draicor <draicor@outlook.com>
2025-05-01 17:09:33 -0400

    01/05/2025 #1
    - Created a new icon for the cycle_movement_speed button (64x64 pixels), replacing the previous ones (took me 6 hours lol) pathetic.
    - Tested it and it runs great.

--- client/components/hud/top_right_hud.gd
+++ client/components/hud/top_right_hud.gd
@@ -1,25 +1,27 @@
 extends Control
 
-@export var walk_texture: CompressedTexture2D = preload("res://assets/icons/icon_walk.png")
-@export var jog_texture: CompressedTexture2D = preload("res://assets/icons/icon_jog.png")
-@export var run_texture: CompressedTexture2D = preload("res://assets/icons/icon_run.png")
+@export var walk_texture: CompressedTexture2D = preload("res://assets/icons/icon_movement_speed_walk.png")
+@export var jog_texture: CompressedTexture2D = preload("res://assets/icons/icon_movement_speed_jog.png")
+@export var run_texture: CompressedTexture2D = preload("res://assets/icons/icon_movement_speed_run.png")
 
-@onready var change_move_speed: Button = $ChangeMoveSpeed
+
+@onready var texture_rect: TextureRect = $TextureRect
+
 
 func _ready() -> void:
-	change_move_speed.icon = run_texture
+	texture_rect.texture = run_texture
+
 
 # Request the server to change the movement speed
 func _on_change_move_speed_button_down() -> void:
 	# Only send the packet to update our speed if we are IDLE
 	if GameManager.player_character.current_animation == GameManager.player_character.ASM.IDLE:
 		if GameManager.player_character.player_speed == 1:
-			change_move_speed.icon = jog_texture
+			texture_rect.texture = jog_texture
 		elif GameManager.player_character.player_speed == 2:
-			change_move_speed.icon = run_texture
+			texture_rect.texture = run_texture
 		elif GameManager.player_character.player_speed == 3:
-			change_move_speed.icon = walk_texture
+			texture_rect.texture = walk_texture
 		
 		# Report it to the rest of the code
 		Signals.ui_change_move_speed_button.emit()
-		
