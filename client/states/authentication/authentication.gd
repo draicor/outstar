7c24d41cbee6cf96049a401047cf02d9933748d0
Draicor <draicor@outlook.com>
2025-05-16 10:48:41 -0400

    16/05/2025 #1
    - Added a gender selection container in the authentication state, to use it during registration, with some validation code too.
    - Working on exporting the mixamo models from blender to godot as glTF, so we can reuse the same skeleton for multiple characters.

--- client/states/authentication/authentication.gd
+++ client/states/authentication/authentication.gd
@@ -2,13 +2,18 @@ extends Node
 
 const packets := preload("res://packets.gd")
 
-@onready var clients_online: Label = $UI/MarginContainer/VBoxContainer/ClientsOnline
-@onready var username_input: LineEdit = $UI/MarginContainer/VBoxContainer/Username
-@onready var password_input: LineEdit = $UI/MarginContainer/VBoxContainer/Password
-@onready var nickname_input: LineEdit = $UI/MarginContainer/VBoxContainer/Nickname
-@onready var status: Label = $UI/MarginContainer/VBoxContainer/Status
-@onready var login_button: Button = $UI/MarginContainer/VBoxContainer/HBoxContainer/LoginButton
-@onready var register_button: Button = $UI/MarginContainer/VBoxContainer/HBoxContainer/RegisterButton
+# Top container
+@onready var clients_online: Label = $UI/MarginContainer/AuthenticationContainer/TopContainer/ClientsOnline
+@onready var status: Label = $UI/MarginContainer/AuthenticationContainer/TopContainer/Status
+@onready var username_input: LineEdit = $UI/MarginContainer/AuthenticationContainer/TopContainer/UsernameInput
+@onready var password_input: LineEdit = $UI/MarginContainer/AuthenticationContainer/TopContainer/PasswordInput
+@onready var nickname_input: LineEdit = $UI/MarginContainer/AuthenticationContainer/TopContainer/NicknameInput
+# Bottom container
+@onready var male_radio_button: CheckBox = $UI/MarginContainer/AuthenticationContainer/BottomContainer/GenderContainer/MaleRadioButton
+@onready var female_radio_button: CheckBox = $UI/MarginContainer/AuthenticationContainer/BottomContainer/GenderContainer/FemaleRadioButton
+@onready var login_button: Button = $UI/MarginContainer/AuthenticationContainer/BottomContainer/ButtonContainer/LoginButton
+@onready var register_button: Button = $UI/MarginContainer/AuthenticationContainer/BottomContainer/ButtonContainer/RegisterButton
+
 
 func _ready() -> void:
 	# Websocket signals
@@ -17,13 +22,19 @@ func _ready() -> void:
 	# Connect the username/password inputs to the login
 	username_input.text_submitted.connect(_on_login_input_text_submitted)
 	password_input.text_submitted.connect(_on_login_input_text_submitted)
+	# Gender selection buttons (ensure none is selected by default)
+	male_radio_button.set_pressed_no_signal(false)
+	female_radio_button.set_pressed_no_signal(false)
+	
 	# Focus on the username field
 	username_input.grab_focus()
+
 
 func _on_websocket_connection_closed() -> void:
 	_update_status("You have been disconnected from the server")
 
-func _on_websocket_packet_received(packet: packets.Packet) -> void:	
+
+func _on_websocket_packet_received(packet: packets.Packet) -> void:
 	if packet.has_request_denied():
 		_update_status(packet.get_request_denied().get_reason())
 	elif packet.has_request_granted():
@@ -33,10 +44,12 @@ func _on_websocket_packet_received(packet: packets.Packet) -> void:
 	elif packet.has_server_metrics():
 		_handle_server_metrics(packet.get_server_metrics())
 
+
 # We need to get the _text from the event but we ignore it because
 # we only care about the event itself, not the text being sent by it
 func _on_login_input_text_submitted(_text: String) -> void:
 	_on_login_button_pressed()
+
 
 func _on_login_button_pressed() -> void:
 	# We validate username and password fields
@@ -59,6 +72,7 @@ func _on_login_button_pressed() -> void:
 	else:
 		_update_status("Logging in...")
 
+
 func _handle_login_success(login_success_packet: packets.LoginSuccess) -> void:
 	# We store the info of our client sent by the server
 	GameManager.client_nickname = login_success_packet.get_nickname()
@@ -67,9 +81,11 @@ func _handle_login_success(login_success_packet: packets.LoginSuccess) -> void:
 	# Move this player to the GAME state
 	GameManager.set_state(GameManager.State.GAME)
 
+
 func _handle_server_metrics(server_metrics_packet: packets.ServerMetrics) -> void:
 	var metrics_players_online := server_metrics_packet.get_players_online()
 	clients_online.text = "Clients online: " + str(metrics_players_online)
+
 
 func _on_register_button_pressed() -> void:
 	# We validate all fields before we even try to talk to the server
@@ -79,6 +95,8 @@ func _on_register_button_pressed() -> void:
 		return
 	if not _password_is_valid(password_input):
 		return
+	if not _gender_is_valid():
+		return
 		
 	# Create the register_request packet
 	var packet := packets.Packet.new()
@@ -87,7 +105,7 @@ func _on_register_button_pressed() -> void:
 	register_request.set_username(username_input.text)
 	register_request.set_nickname(nickname_input.text)
 	register_request.set_password(password_input.text)
-	register_request.set_gender("male")
+	register_request.set_gender(_get_selected_gender())
 	# Serialize and send it to the server
 	var err := WebSocket.send(packet)
 	if err:
@@ -95,8 +113,10 @@ func _on_register_button_pressed() -> void:
 	else:
 		_update_status("Creating user...")
 
+
 func _handle_request_granted_packet() -> void:
 	_update_status("Registration successful")
+
 
 func _name_is_valid(input: LineEdit) -> bool:
 	if input.text.is_empty():
@@ -109,6 +129,7 @@ func _name_is_valid(input: LineEdit) -> bool:
 		return false
 	
 	return true
+
 
 func _password_is_valid(input: LineEdit) -> bool:
 	if input.text.is_empty():
@@ -126,5 +147,42 @@ func _password_is_valid(input: LineEdit) -> bool:
 	
 	return true
 
+
+# Validates that the gender is properly set for register
+func _gender_is_valid() -> bool:
+	if _get_selected_gender().is_empty():
+		_update_status("Gender not selected")
+		return false
+	
+	return true
+
+
+# Updates the status label
 func _update_status(text: String) -> void:
 	status.text = text
+
+
+# Signal connected through the inspector
+func _on_male_radio_button_toggled(toggled_on: bool) -> void:
+	# If we pressed the male button
+	if toggled_on and male_radio_button.button_pressed:
+		# Disable the female button
+		female_radio_button.set_pressed_no_signal(false)
+
+
+# Signal connected through the inspector
+func _on_female_radio_button_toggled(toggled_on: bool) -> void:
+	# If we pressed the female button
+	if toggled_on and female_radio_button.button_pressed:
+		# Disable the male button
+		male_radio_button.set_pressed_no_signal(false)
+
+
+# Helper function to return the selected gender from the radio buttons
+func _get_selected_gender() -> String:
+	if male_radio_button.button_pressed:
+		return "male"
+	elif female_radio_button.button_pressed:
+		return "female"
+	
+	return ""
