30f4a51c2305bcb21ac63d26197e234b6690f1a7
Draicor <draicor@outlook.com>
2025-06-17 15:11:52 -0400

    17/06/2025 #2
    - Removed unused animations in the locomotion. (male_jog, rifle_aim_run, rifle_down_jog).
    - Edited sounds to create a reload animation and dry fire for the projectile rifle.
    - Extended the PlayerAudio to play the reload sounds and added the animation sounds to the animation timeline.

--- client/classes/player/player_audio.gd
+++ client/classes/player/player_audio.gd
@@ -1,8 +1,14 @@
 extends Node3D
 class_name PlayerAudio
 
-# Preload sound resources
-const RIFLE_FIRE_SINGLE = preload("res://assets/sounds/sfx/rifle_fire_single.wav")
+# Projectile rifle fire sounds
+const PROJECTILE_RIFLE_FIRE_SINGLE = preload("res://assets/sounds/sfx/projectile_rifle_fire_single.wav")
+const PROJECTILE_RIFLE_DRY_FIRE_SINGLE = preload("res://assets/sounds/sfx/projectile_rifle_dry_fire_single.wav")
+# Projectile rifle reload sounds
+const PROJECTILE_RIFLE_REMOVE_MAGAZINE = preload("res://assets/sounds/sfx/projectile_rifle_remove_magazine.wav")
+const PROJECTILE_RIFLE_INSERT_MAGAZINE = preload("res://assets/sounds/sfx/projectile_rifle_insert_magazine.wav")
+const PROJECTILE_RIFLE_CHARGING_HANDLE = preload("res://assets/sounds/sfx/projectile_rifle_charging handle.wav")
+
 
 # Preloading scripts
 const Player: GDScript = preload("res://objects/player/player.gd")
@@ -10,9 +16,14 @@ const Player: GDScript = preload("res://objects/player/player.gd")
 # Internal variables
 var player: Player = null # Our parent node
 
-# AudioStreamPlayer3D Pool
+# AudioStreamPlayer3D Fire pool
 var audio_players_3d: Array[AudioStreamPlayer3D] = []
 var current_audio_players_3d_index: int = 0
+# AudioStreamPlayer3D Reload pool
+var remove_magazine_audio_player_3d: AudioStreamPlayer3D
+var insert_magazine_audio_player_3d: AudioStreamPlayer3D
+var charging_handle_audio_player_3d: AudioStreamPlayer3D
+
 
 # Setup
 const POOL_SIZE: int = 5 # Number of overlapping sounds
@@ -27,6 +38,11 @@ var animation_events: Dictionary[String, Array] = {
 	"rifle/rifle_aim_fire_single_fast": [
 		{"time": 0.1, "method": "play_3d_sound", "args": []},
 	],
+	"rifle/rifle_aim_reload_fast": [
+		{"time": 0.7, "method": "play_projectile_rifle_remove_magazine", "args": []},
+		{"time": 1.6, "method": "play_projectile_rifle_insert_magazine", "args": []},
+		{"time": 2.1, "method": "play_projectile_rifle_charging_handle", "args": []},
+	]
 }
 
 
@@ -35,11 +51,11 @@ func _ready() -> void:
 	await get_parent().ready
 	player = get_parent()
 	
-	# Create pool of audio players
+	# Create pool of audio players for rifle pool
 	for i in POOL_SIZE:
 		var new_audio_player_3d = AudioStreamPlayer3D.new()
 		# Setup this audio player
-		new_audio_player_3d.stream = RIFLE_FIRE_SINGLE
+		new_audio_player_3d.stream = PROJECTILE_RIFLE_DRY_FIRE_SINGLE
 		new_audio_player_3d.volume_db = BASE_VOLUME
 		new_audio_player_3d.max_distance = MAX_DISTANCE
 		new_audio_player_3d.bus = "SFX"
@@ -47,7 +63,12 @@ func _ready() -> void:
 		add_child(new_audio_player_3d)
 		audio_players_3d.append(new_audio_player_3d)
 	
-	# Connect to animation player signals
+	# Create reload audio players
+	remove_magazine_audio_player_3d = create_single_audio_player_3d(PROJECTILE_RIFLE_REMOVE_MAGAZINE, 8.0, 15.0, "SFX")
+	insert_magazine_audio_player_3d = create_single_audio_player_3d(PROJECTILE_RIFLE_INSERT_MAGAZINE, 8.0, 15.0, "SFX")
+	charging_handle_audio_player_3d = create_single_audio_player_3d(PROJECTILE_RIFLE_CHARGING_HANDLE, 15.0, 20.0, "SFX")
+	
+	# Connect to animation player signals to check every frame to call anim events
 	player.player_animator.animation_player.connect("animation_started", _on_animation_started)
 	player.player_animator.animation_player.connect("animation_finished", _on_animation_finished)
 
@@ -102,3 +123,37 @@ func check_animation_events() -> void:
 			if current_time < event["time"] + 0.1: # 100ms tolerance
 				callv(event["method"], event["args"])
 				triggered_events[event_id] = true
+
+
+func create_single_audio_player_3d(stream: AudioStream, volume_db: float, max_distance: float, bus: String) -> AudioStreamPlayer3D:
+	var audio_player_3d: AudioStreamPlayer3D = AudioStreamPlayer3D.new()
+	audio_player_3d.stream = stream
+	audio_player_3d.volume_db = volume_db
+	audio_player_3d.max_distance = max_distance
+	audio_player_3d.bus = bus
+	add_child(audio_player_3d)
+	return audio_player_3d
+
+
+func play_projectile_rifle_remove_magazine() -> void:
+	if remove_magazine_audio_player_3d.playing:
+		return # Don't interrupt ongoing sound
+	
+	remove_magazine_audio_player_3d.pitch_scale = randf_range(0.95, 1.05)
+	remove_magazine_audio_player_3d.play()
+
+
+func play_projectile_rifle_insert_magazine() -> void:
+	if insert_magazine_audio_player_3d.playing:
+		return # Don't interrupt ongoing sound
+	
+	insert_magazine_audio_player_3d.pitch_scale = randf_range(0.95, 1.05)
+	insert_magazine_audio_player_3d.play()
+
+
+func play_projectile_rifle_charging_handle() -> void:
+	if charging_handle_audio_player_3d.playing:
+		return # Don't interrupt ongoing sound
+	
+	charging_handle_audio_player_3d.pitch_scale = randf_range(0.98, 1.02)
+	charging_handle_audio_player_3d.play()
